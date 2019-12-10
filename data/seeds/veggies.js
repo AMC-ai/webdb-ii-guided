0ae0ee3f79444426598fd018c5fb705aee35ccc4
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-12-10 14:21:46 -0500

    finished lecture added missing notes

--- data/seeds/veggies.js
+++ data/seeds/veggies.js
@@ -1,9 +1,14 @@
 
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('veggies').del()
+  return
+  // REMEMBER TO CHANGE THE TABLE NAME IN BOTH PLACES
+  knex('veggies')
+    // .del() // does not reset the id
+    .truncate() // empties the table and resets the primary key (id)
     .then(function () {
       // Inserts seed entries
+      // CHANGE THE TABLE NAME
       return knex('veggies').insert([
         { name: 'broccoli', like_it: 1 },
         { name: 'sprouts', like_it: 0 },
--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,25 @@
 // Update with your config settings.
 
+/*
+
+this file contains instructions knex needs to connect
+to the database
+
+Each key on the exported object has the configuration
+for a database environment (dev, production, testing, etc)
+
+The knex CLI will use the configuration on this file to find the DB.
+
+The knex CLI will use the "development" environment by default, but
+we can override that behavior.
+
+*/
+
 module.exports = {
 
   // teaches knex to connect to the development database
   development: {
-    client: 'sqlite3',
+    client: 'sqlite3', // the DB driver
     useNullAsDefault: true, //only needed when using SQLite3
     connection: {
       filename: './data/produce.db3' // where is the database
